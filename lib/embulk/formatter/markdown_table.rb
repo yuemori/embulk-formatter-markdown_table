5c33828a07c3fb8605d40567c0486ca1d9bf4b8d
yuemori <yuemori@aiming-inc.com>
2016-02-11 23:29:46 +0900

    First Implement

--- lib/embulk/formatter/markdown_table.rb
+++ lib/embulk/formatter/markdown_table.rb
@@ -3,12 +3,6 @@ module Embulk
 
     class MarkdownTable < FormatterPlugin
       Plugin.register_formatter("markdown_table", self)
-      VALID_ENCODINGS = %w(UTF-8 UTF-16LE UTF-32BE UTF-32LE UTF-32BE)
-      NEWLINES = {
-        'CRLF' => "\r\n",
-        'LF' => "\n",
-        'CR' => "\r"
-      }
 
       def self.join_texts((*inits,last), opt = {})
         delim = opt[:delimiter] || ', '
@@ -17,28 +11,14 @@ module Embulk
       end
 
       def self.transaction(config, schema, &control)
-        task = {
-          'encoding' => config.param('encoding', :string, default: 'UTF-8'),
-          'newline' => config.param('newline', :string, default: 'LF'),
-          'date_format' => config.param('date_format', :string, default: nil),
-          'timezone' => config.param('timezone', :string, default: nil )
-        }
-
-        encoding = task['encoding'].upcase
-        raise "encoding must be one of #{join_texts(VALID_ENCODINGS)}" unless VALID_ENCODINGS.include?(encoding)
-
-        newline = task['newline'].upcase
-        raise "newline must be one of #{join_texts(NEWLINES.keys)}" unless NEWLINES.has_key?(newline)
+        task = {}
 
         yield(task)
       end
 
       def init
-        @encoding = task['encoding'].upcase
-        @newline = NEWLINES[task['newline'].upcase]
-
-        # your data
-        @current_file == nil
+        @header_print = true
+        @current_file = nil
         @current_file_size = 0
       end
 
@@ -50,8 +30,16 @@ module Embulk
           if @current_file == nil || @current_file_size > 32*1024
             @current_file = file_output.next_file
             @current_file_size = 0
+            @header_print = true
           end
-          @current_file.write "|mydata|"
+
+          if @header_print
+            @current_file.write "|#{page.schema.map(&:name).join('|')}|\n"
+            @current_file.write "|#{(["---"] * page.schema.size).join('|')}|\n"
+            @header_print = false
+          end
+
+          file_output.write "|#{record.join('|')}|\n"
         end
       end
 
